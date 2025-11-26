44759801adef29860e18d632c2108541a34f19b2
Derin Dickson Vaca <derin.dickson.vaca.camacho@gmail.com>
2025-11-25 21:59:05 -0400

    Add clase 3

---
+++ src/infrastructure/repositories/database/product.mongo.repository.js
@@ -0,0 +1,12 @@
+const ProductModel = require('./models/product.model');
+ 
+class ProductMongoRepository { // ¡Implementa la misma interfaz!
+    async getAll() {
+        return ProductModel.find();
+    }
+    async create(productData) {
+        return ProductModel.create(productData);
+    }
+}
+module.exports = ProductMongoRepository;
+ 
--- src/presentation/routes/product.routes.js
+++ src/presentation/routes/product.routes.js
@@ -3,9 +3,9 @@ const ProductController = require('../controller/product.controller');
 
 // Esta es la "Inyección de Dependencias" manual
 const ProductService = require('../../application/use-cases/product.service');
-const MockProductRepository = require('../../infrastructure/repositories/product.mock.repository');
+//const MockProductRepository = require('../../infrastructure/repositories/product.mock.repository');
 
-const productRepository = new MockProductRepository();
+//const productRepository = new MockProductRepository();
 const productService = new ProductService(productRepository);
 const productController = new ProductController(productService);
 
