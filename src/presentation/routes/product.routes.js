c9da6e2fdb32b157dc583123694e6412eacd0381
Derin Dickson Vaca <derin.dickson.vaca.camacho@gmail.com>
2025-12-01 17:43:09 -0400

    Cambios

--- src/presentation/routes/product.routes.js
+++ src/presentation/routes/product.routes.js
@@ -3,8 +3,10 @@ const ProductController = require('../controller/product.controller');
 
 // Esta es la "Inyección de Dependencias" manual
 const ProductService = require('../../application/use-cases/product.service');
+//const MockProductRepository = require('../../infrastructure/repositories/product.mock.repository');
 
-const ProductMongoRepository = require('../../infrastructure/repositories/database/mongo/product.mongo.repository');
+//const productRepository = new MockProductRepository();
+require('../../infrastructure/database/mongo/product.mongo.repository');
 const productRepository = new ProductMongoRepository();
 
 const productService = new ProductService(productRepository);
@@ -12,9 +14,6 @@ const productController = new ProductController(productService);
 
 const router = Router();
 router.get('/', productController.getAll);
-router.get('/:id', productController.getById);
 router.post('/', productController.create);
-router.put('/:id', productController.update);
-router.delete('/:id', productController.delete);
 
 module.exports = router;
