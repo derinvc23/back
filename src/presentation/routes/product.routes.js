57b4423b327d7edf77f86cb7eb46812a8b7c4a08
Derin Dickson Vaca <derin.dickson.vaca.camacho@gmail.com>
2025-12-01 17:41:52 -0400

    Add ajustes clase III

--- src/presentation/routes/product.routes.js
+++ src/presentation/routes/product.routes.js
@@ -3,14 +3,18 @@ const ProductController = require('../controller/product.controller');
 
 // Esta es la "Inyección de Dependencias" manual
 const ProductService = require('../../application/use-cases/product.service');
-//const MockProductRepository = require('../../infrastructure/repositories/product.mock.repository');
 
-//const productRepository = new MockProductRepository();
+const ProductMongoRepository = require('../../infrastructure/repositories/database/mongo/product.mongo.repository');
+const productRepository = new ProductMongoRepository();
+
 const productService = new ProductService(productRepository);
 const productController = new ProductController(productService);
 
 const router = Router();
 router.get('/', productController.getAll);
+router.get('/:id', productController.getById);
 router.post('/', productController.create);
+router.put('/:id', productController.update);
+router.delete('/:id', productController.delete);
 
 module.exports = router;
